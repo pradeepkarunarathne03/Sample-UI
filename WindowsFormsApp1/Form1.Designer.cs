9264ec4541ce8833c125a8b6fee4885a037008ca
pradeepkarunarathne03 <pradeepkarunarathne03@gmail.com>
2024-07-02 18:23:19 +0530

    2024-07-02 - UI Sample Program

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirstNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSecondNumber = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgvEmployeeSalaryData = new System.Windows.Forms.DataGridView();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Salary = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployeeSalaryData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtFirstNumber
+            // 
+            this.txtFirstNumber.Location = new System.Drawing.Point(131, 30);
+            this.txtFirstNumber.Name = "txtFirstNumber";
+            this.txtFirstNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstNumber.TabIndex = 0;
+            this.txtFirstNumber.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Second Number";
+            // 
+            // txtSecondNumber
+            // 
+            this.txtSecondNumber.Location = new System.Drawing.Point(131, 66);
+            this.txtSecondNumber.Name = "txtSecondNumber";
+            this.txtSecondNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtSecondNumber.TabIndex = 2;
+            this.txtSecondNumber.Text = "0";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(131, 106);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 4;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(156, 132);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Result";
+            // 
+            // dgvEmployeeSalaryData
+            // 
+            this.dgvEmployeeSalaryData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmployeeSalaryData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name,
+            this.Age,
+            this.Salary});
+            this.dgvEmployeeSalaryData.Location = new System.Drawing.Point(400, 47);
+            this.dgvEmployeeSalaryData.Name = "dgvEmployeeSalaryData";
+            this.dgvEmployeeSalaryData.Size = new System.Drawing.Size(540, 301);
+            this.dgvEmployeeSalaryData.TabIndex = 7;
+            // 
+            // Name
+            // 
+            this.Name.HeaderText = "Name";
+            this.Name.Name = "Name";
+            this.Name.Width = 200;
+            // 
+            // Age
+            // 
+            this.Age.HeaderText = "Age";
+            this.Age.Name = "Age";
+            // 
+            // Salary
+            // 
+            this.Salary.HeaderText = "Salary";
+            this.Salary.Name = "Salary";
+            this.Salary.Width = 120;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(434, 381);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Show Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvEmployeeSalaryData);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSecondNumber);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFirstNumber);
+            
+            this.Text = "Calculater";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployeeSalaryData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirstNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSecondNumber;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgvEmployeeSalaryData;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Age;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Salary;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calculate()
+        {
+            int firstNumber = int.Parse(txtFirstNumber.Text);
+            int secondNumber = int.Parse(txtSecondNumber.Text); 
+
+            MyCalculation objMyCalculation = new MyCalculation();
+            int result = objMyCalculation.CalculateSquare(firstNumber, secondNumber);
+
+            txtResult.Text = result.ToString();
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int firstNumber = int.Parse(txtFirstNumber.Text);
+            int secondNumber = int.Parse(txtSecondNumber.Text);
+            
+            if(firstNumber > 0 && secondNumber > 0)
+            {
+                Calculate();
+
+            }
+            else
+            {
+                MessageBox.Show("Data Not Valid");
+            }
+        }
+
+        public class EmployeeSalary
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public int Salary { get; set; }
+        }
+
+
+        private void readDataGridViewData()
+        {
+            //for (int i = 0; i < dgvEmployeeSalaryData.RowCount - 1; i++)
+            //{                 
+            //    string Name = dgvEmployeeSalaryData.Rows[i].Cells["Name"].Value.ToString();
+            //    int Age = int.Parse(dgvEmployeeSalaryData.Rows[i].Cells["Age"].Value.ToString());
+            //    int Salary = int.Parse(dgvEmployeeSalaryData.Rows[i].Cells["Salary"].Value.ToString());
+            //    Console.WriteLine($"Index is {i} - {Name} - {Age} - {Salary}");
+            //}
+
+            foreach (DataGridViewRow row in dgvEmployeeSalaryData.Rows)
+            {
+                Console.WriteLine(row.Cells["Name"].Value);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            readDataGridViewData();
+        }
+    }
+}
